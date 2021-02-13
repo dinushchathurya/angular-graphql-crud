528833f6b34ec096dd26cfae8b730896ffbc6cc3
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-13 21:26:29 +0530

    :sparkles: Create schema file

---
+++ backend/graphql/schema.js
@@ -0,0 +1,18 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+    type Quote {
+        _id: ID!
+        quote: String!
+        author: String!
+    }
+    type QuoteData {
+        quotes: [Quote!]!
+    }
+    type RootQuery {
+        quotes: QuoteData!
+    }
+    schema {
+        query: RootQuery
+    }
+`);
